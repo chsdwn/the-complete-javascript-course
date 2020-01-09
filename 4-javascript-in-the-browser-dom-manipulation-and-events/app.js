112a35d871ee0bd470a2aeb8194f2acac1457915
chsdwn <hulusikafalier@gmail.com>
2020-01-09 17:26:17 +0300

    Implementing Our 'Hold' Funtion and the DRY Principle.

--- 4-javascript-in-the-browser-dom-manipulation-and-events/app.js
+++ 4-javascript-in-the-browser-dom-manipulation-and-events/app.js
@@ -22,29 +22,46 @@ document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('.btn-roll').addEventListener('click', function() { // Anonymous function
+document.querySelector('.btn-roll').addEventListener('click', function () { // Anonymous function
   var dice = Math.floor(Math.random() * 6) + 1;
 
   var diceDOM = document.querySelector('.dice');
   diceDOM.style.display = 'block';
   diceDOM.src = 'dice-' + dice + '.png';
 
-  if(dice !== 1) {
+  if (dice !== 1) {
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    // Removes active class from element that has class="player-0-panel"
-    // document.querySelector('.player-0-panel').classList.remove('active');
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
   }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  scores[activePlayer] += roundScore;
+
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  if (scores[activePlayer] > 5) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    nextPlayer();
+  }
+
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
