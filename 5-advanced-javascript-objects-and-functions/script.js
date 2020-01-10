17e4ce89bcf8bb1d4df28d21424b03867887a65d
chsdwn <hulusikafalier@gmail.com>
2020-01-10 16:32:56 +0300

    Closures.

--- 5-advanced-javascript-objects-and-functions/script.js
+++ 5-advanced-javascript-objects-and-functions/script.js
@@ -1,15 +1,31 @@
-function game() {
-  var score = Math.random() * 10;
-  console.log(score >= 5);
+function retirement(retirementAge) {
+  var a = ' years left until retirement.'
+  return function (yearOfBirth) {
+    var age = 2020 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  }
 }
-game();
 
-(function () {
-  var score = Math.random() * 10;
-  console.log(score >= 5);
-})();
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67)
 
-(function (goodLuck) {
-  var score = Math.random() * 10;
-  console.log(score >= 5 - goodLuck);
-})(5);
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+function interviewQuestion(job) {
+  return function (name) {
+    var question;
+    if (job === 'designer') {
+      question = name + ', can you please explain what UX design is?';
+    } else if (job === 'teacher') {
+      question = 'What subject do you teach, ' + name + '?';
+    } else {
+      question = 'Hello ' + name + ', what do you do?';
+    }
+    console.log(question);
+  }
+}
+
+interviewQuestion('designer')('John');
