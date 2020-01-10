b88f7b0b1a27670940ed5fa0bcd7ecd2f6051c9d
chsdwn <hulusikafalier@gmail.com>
2020-01-10 14:43:49 +0300

    Creating Objects Object.create.

--- 5-advanced-javascript-objects-and-functions/script.js
+++ 5-advanced-javascript-objects-and-functions/script.js
@@ -1,29 +1,22 @@
-var john = {
-  name: 'John',
-  yearOfBirth: 1990,
-  job: 'teacher'
-};
-
-var Person = function(name, yearOfBirth, job) {
-  this.name = name;
-  this.yearOfBirth = yearOfBirth;
-  this.job = job;
-};
-
-Person.prototype.calculateAge = function() {
-  console.log(2020 - this.yearOfBirth);
+var personProto = {
+  calculateAge: function () {
+    console.log(2019 - this.yearOfBirth);
+  }
 }
 
-Person.prototype.lastName = 'Smith';
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
 
-var john = new Person('John', 1990, 'teacher');
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
-
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
-
-console.log(john.lastName);
-console.log(jane.lastName);
-console.log(mark.lastName);
+var jane = Object.create(personProto, {
+  name: {
+    value: 'Jane'
+  },
+  yearOfBirth: {
+    value: 1969
+  },
+  job: {
+    value: 'designer'
+  }
+});
