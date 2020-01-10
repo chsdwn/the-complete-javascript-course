74383c4be69e53f3fa37f135a58ecf4ffd2671b4
chsdwn <hulusikafalier@gmail.com>
2020-01-10 15:55:49 +0300

    First Class Functions Passing Functions as Arguments.

--- 5-advanced-javascript-objects-and-functions/script.js
+++ 5-advanced-javascript-objects-and-functions/script.js
@@ -1,29 +1,32 @@
-var a = 23;
-var b = a;
-a = 46;
-// b = 23
+var years = [1990, 1965, 1937, 2005, 1998];
 
-var obj1 = {
-  name: 'John',
-  age: 26
-};
-
-var obj2 = obj1;
-obj1.age = 30;
-// obj1.age = 30
-// obj2.age = 30
-
-var age = 27;
-var obj = {
-  name: 'Jonas',
-  city: 'Lisbon'
-};
-
-function change(a, b) {
-  a = 30;
-  b.city = 'San Francisco';
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
 }
 
-change(age, obj);
-// age = 27
-// obj.city = 'San Francisco'
+function calculateAge(el) {
+  return 2020 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81){
+    return Math.round(206.9 - (0.67 * el));  
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calculateAge);
+console.log(ages);
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(fullAges);
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(rates);
