7218ae87a51b9eb77f1bb6357b8d8a1057ed76db
chsdwn <hulusikafalier@gmail.com>
2020-01-10 16:03:01 +0300

    First Class Functions, Functions Returning Functions.

--- 5-advanced-javascript-objects-and-functions/script.js
+++ 5-advanced-javascript-objects-and-functions/script.js
@@ -1,32 +1,24 @@
-var years = [1990, 1965, 1937, 2005, 1998];
-
-function arrayCalc(arr, fn) {
-  var arrRes = [];
-  for (var i = 0; i < arr.length; i++) {
-    arrRes.push(fn(arr[i]));
-  }
-  return arrRes;
-}
-
-function calculateAge(el) {
-  return 2020 - el;
-}
-
-function isFullAge(el) {
-  return el >= 18;
-}
-
-function maxHeartRate(el) {
-  if (el >= 18 && el <= 81){
-    return Math.round(206.9 - (0.67 * el));  
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What subject do you teach, ' + name + '?');
+    }
   } else {
-    return -1;
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    }
   }
 }
 
-var ages = arrayCalc(years, calculateAge);
-console.log(ages);
-var fullAges = arrayCalc(ages, isFullAge);
-console.log(fullAges);
-var rates = arrayCalc(ages, maxHeartRate);
-console.log(rates);
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+
+interviewQuestion('teacher')('Mark');
