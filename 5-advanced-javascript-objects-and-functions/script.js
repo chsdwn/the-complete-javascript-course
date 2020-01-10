11fc357f1b8566ed25acd5c4602cb7c5cbe5ed26
chsdwn <hulusikafalier@gmail.com>
2020-01-10 14:51:48 +0300

    Primitives vs Objects.

--- 5-advanced-javascript-objects-and-functions/script.js
+++ 5-advanced-javascript-objects-and-functions/script.js
@@ -1,22 +1,29 @@
-var personProto = {
-  calculateAge: function () {
-    console.log(2019 - this.yearOfBirth);
-  }
+var a = 23;
+var b = a;
+a = 46;
+// b = 23
+
+var obj1 = {
+  name: 'John',
+  age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+// obj1.age = 30
+// obj2.age = 30
+
+var age = 27;
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon'
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = 'San Francisco';
 }
 
-var john = Object.create(personProto);
-john.name = 'John';
-john.yearOfBirth = 1990;
-john.job = 'teacher';
-
-var jane = Object.create(personProto, {
-  name: {
-    value: 'Jane'
-  },
-  yearOfBirth: {
-    value: 1969
-  },
-  job: {
-    value: 'designer'
-  }
-});
+change(age, obj);
+// age = 27
+// obj.city = 'San Francisco'
