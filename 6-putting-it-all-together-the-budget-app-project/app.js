3cbab8b942cb3c53cfa8fd6f36c83d6531e836d8
chsdwn <hulusikafalier@gmail.com>
2020-01-11 11:39:31 +0300

    Reading Input Data.

--- 6-putting-it-all-together-the-budget-app-project/app.js
+++ 6-putting-it-all-together-the-budget-app-project/app.js
@@ -1,22 +1,43 @@
 // BUDGET CONTROLLER
-var budgetController = (function() {
+var budgetController = (function () {
 
 })();
 
 // UI CONTROLLER
-var UIController = (function() {
+var UIController = (function () {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
 
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // returns inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 // GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) {
-  var ctrlAddItem = function() {
+var controller = (function (budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
 
+  var ctrlAddItem = function () {
+    var input = UICtrl.getInput();
+    console.log(input);
   };
-  
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
-  document.addEventListener('keypress', function(event) {
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function (event) {
     // event.which for older browsers
     if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
