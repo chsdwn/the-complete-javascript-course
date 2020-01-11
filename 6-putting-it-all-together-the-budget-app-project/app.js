95fb2d48042b8a189670e8e96bb3c9871fc7dc23
chsdwn <hulusikafalier@gmail.com>
2020-01-11 18:48:26 +0300

    Clearing Our Input Fields.

--- 6-putting-it-all-together-the-budget-app-project/app.js
+++ 6-putting-it-all-together-the-budget-app-project/app.js
@@ -105,6 +105,17 @@ var UIController = (function () {
       newHtml = newHtml.replace('%value%', obj.value);
 
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+    clearFields: function () {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function (current, index, array) {
+        current.value = '';
+      });
+      fieldsArr[0].focus();
     }
   };
 })();
@@ -130,6 +141,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     input = UICtrl.getInput();
     newItem = budgetController.addItem(input.type, input.description, input.value);
     UICtrl.addListItem(newItem, input.type);
+    UICtrl.clearFields();
   };
 
   return {
