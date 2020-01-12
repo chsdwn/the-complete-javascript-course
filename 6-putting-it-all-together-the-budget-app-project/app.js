b7af75b931e9ab888201125ef7a3c4bc593bd4ca
chsdwn <hulusikafalier@gmail.com>
2020-01-12 14:31:45 +0300

    Deleting an Item from Our Budget Controller.

--- 6-putting-it-all-together-the-budget-app-project/app.js
+++ 6-putting-it-all-together-the-budget-app-project/app.js
@@ -72,6 +72,19 @@ var budgetController = (function () {
         percentage: data.percentage
       }
     },
+    deleteItem: function (type, id) {
+      var ids, index;
+
+      ids = data.allItems[type].map(function (current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+    },
     testing: function () {
       console.log(data);
     }
@@ -215,9 +228,9 @@ var controller = (function (budgetCtrl, UICtrl) {
     if (itemID) {
       splitID = itemID.split('-');
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
 
-
+      budgetCtrl.deleteItem(type, ID);
     }
   }
 
