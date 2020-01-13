e86074eb6ebcd2fd9f6a0dc2ded8ed8c70c31ecd
chsdwn <hulusikafalier@gmail.com>
2020-01-13 14:51:46 +0300

    Strings in ES6 ES2015.

--- 7-next-generation-javascript-intro-to-es6-es2015/script.js
+++ 7-next-generation-javascript-intro-to-es6-es2015/script.js
@@ -1,12 +1,21 @@
-// Lecture: Blocks and IIFEs
+// Lecture: Strings
+
+let firstName = 'john';
+let lastName = 'smith';
+const yearOfBirth = 1990;
+function calcAge(year) {
+  return 2020 - year;
+}
 
 // ES5
-(function () {
-  var c = 3;
-})();
+console.log('This is ' + firstName + ' ' + lastName + '. ' + calcAge(yearOfBirth) + ' years old.');
 
 // ES6
-{
-  const a = 1;
-  let b = 2;
-}
+console.log(`This is ${firstName} ${lastName}. ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('j'));
+console.log(n.endsWith('h'));
+console.log(n.includes('oh'));
+console.log(n.includes(' smi'));
+console.log(`${firstName} `.repeat(5));
