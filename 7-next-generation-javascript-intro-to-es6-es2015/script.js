fc1d65af7b404ef6e3e77064c76c092918cf035f
chsdwn <hulusikafalier@gmail.com>
2020-01-13 14:43:29 +0300

    Blocks and IIFEs.

--- 7-next-generation-javascript-intro-to-es6-es2015/script.js
+++ 7-next-generation-javascript-intro-to-es6-es2015/script.js
@@ -1,44 +1,12 @@
-// Lecture: let and const
+// Lecture: Blocks and IIFEs
 
 // ES5
-var name5 = 'Ayşe Veli';
-var age5 = 23;
-name5 = 'Ayşe Ali';
+(function () {
+  var c = 3;
+})();
 
 // ES6
-const name6 = 'Ayşe Veli';
-let age6 = 23;
-
-/********************************************/
-
-// ES5
-function driversLicence5(passedTest) {
-  if (passedTest) {
-    var firstName = 'John';
-    var yearOfBirth = 1990;
-  }
-
-  console.log(firstName + ' ' + yearOfBirth);
+{
+  const a = 1;
+  let b = 2;
 }
-driversLicence5(true);
-
-// ES6
-function driversLicence6(passedTest) {
-  let firstName;
-  const yearOfBirth = 1990;
-
-  if (passedTest) {
-    firstName = 'John';
-  }
-
-  console.log(firstName + ' ' + yearOfBirth);
-}
-driversLicence6(true);
-
-/********************************************/
-
-let i = 23;
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
-console.log(i);
