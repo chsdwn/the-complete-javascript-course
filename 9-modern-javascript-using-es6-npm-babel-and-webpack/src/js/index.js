029a1a46cc32550140c8f9e3be2381458b9366f2
chsdwn <hulusikafalier@gmail.com>
2020-01-21 15:58:01 +0300

    Building the Search Model.

--- 9-modern-javascript-using-es6-npm-babel-and-webpack/src/js/index.js
+++ 9-modern-javascript-using-es6-npm-babel-and-webpack/src/js/index.js
@@ -1,14 +1,5 @@
-import axios from 'axios';
-// d4f9ceaa577e45288b641d3b0c08c0bd
+import Search from './models/Search';
 
-async function getResults(query) {
-  const key = 'd4f9ceaa577e45288b641d3b0c08c0bd';
-  try {
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch(error) {
-    console.log(error);
-  }
-}
-getResults('pasta');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
