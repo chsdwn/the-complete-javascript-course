54dfc0f3afa92b5b320ce0e2a00b8b6ab6e47f6f
chsdwn <hulusikafalier@gmail.com>
2020-01-21 16:09:47 +0300

    Building the Search Controller.

--- 9-modern-javascript-using-es6-npm-babel-and-webpack/src/js/index.js
+++ 9-modern-javascript-using-es6-npm-babel-and-webpack/src/js/index.js
@@ -1,5 +1,24 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/* Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {}
+
+const controlSearch = async () => {
+  const query = 'pizza';
+
+  if (query) {
+    state.search = new Search(query);
+    await state.search.getResults();
+    console.log(state.search.result);
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
