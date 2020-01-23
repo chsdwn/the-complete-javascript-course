6e4f46502835a79ebe0e3407b63bba9587b301a2
chsdwn <hulusikafalier@gmail.com>
2020-01-23 11:57:32 +0300

    Rendering an AJAX Loading Spinner.

--- 9-modern-javascript-using-es6-npm-babel-and-webpack/src/js/index.js
+++ 9-modern-javascript-using-es6-npm-babel-and-webpack/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /* Global state of the app
  * - Search object
@@ -12,16 +12,17 @@ const state = {};
 
 const controlSearch = async () => {
   const query = searchView.getInput();
-  console.log(query);
 
   if (query) {
     state.search = new Search(query);
 
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     await state.search.getResults();
 
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 };
