9bc101c2ccbf0f4b5de4cb684f94fdcf314936d1
chsdwn <hulusikafalier@gmail.com>
2020-01-24 19:00:45 +0300

    Building the Recipe View. Part 2

--- 9-modern-javascript-using-es6-npm-babel-and-webpack/src/js/index.js
+++ 9-modern-javascript-using-es6-npm-babel-and-webpack/src/js/index.js
@@ -64,6 +64,8 @@ const controlRecipe = async () => {
     recipeView.clearRecipe();
     renderLoader(elements.recipe);
 
+    if (state.search) searchView.highlightSelected(id);
+
     state.recipe = new Recipe(id);
 
     try {
