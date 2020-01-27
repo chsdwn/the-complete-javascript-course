82716ae277e1fc643cfc116f4dd323134e9dd0e6
chsdwn <hulusikafalier@gmail.com>
2020-01-27 13:30:31 +0300

    The Laptop Store Project. Part 2

--- 12-bonus-a-node.js-crash-course/index.js
+++ 12-bonus-a-node.js-crash-course/index.js
@@ -12,11 +12,31 @@ const server = http.createServer((req, res) => {
   // Routing
   if (pathname === '/products' || pathname === '/') {
     res.writeHead(200, {'Content-type': 'text/html'});
-    res.end('products page');
+    
+    fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+      let overviewOutput = data;
+
+      fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+        const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+        overviewOutput = overviewOutput.replace('${%CARDS%}', cardsOutput);
+        res.end(overviewOutput);
+      });
+    });
 
   } else if (pathname === '/laptop' && id < laptopData.length) {
     res.writeHead(200, {'Content-type': 'text/html'});
-    res.end(`laptop page, id: ${id}`);
+    
+    fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+      const laptop = laptopData[id];
+      const output = replaceTemplate(data, laptop);
+      res.end(output);
+    });
+
+  } else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathname)) {
+    fs.readFile(`${__dirname}/data/img${pathname}`, (err, data) => {
+      res.writeHead(200, {'Content-type': 'image/jpg'});
+      res.end(data);
+    });
 
   } else {
     res.writeHead(404, {'Content-type': 'text/html'});
@@ -27,3 +47,16 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
   console.log('listening');
 });
+
+function replaceTemplate(html, laptop) {
+  let output = html.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+  output = output.replace(/{%IMAGE%}/g, laptop.image);
+  output = output.replace(/{%PRICE%}/g, laptop.price);
+  output = output.replace(/{%SCREEN%}/g, laptop.screen);
+  output = output.replace(/{%CPU%}/g, laptop.cpu);
+  output = output.replace(/{%STORAGE%}/g, laptop.storage);
+  output = output.replace(/{%RAM%}/g, laptop.ram);
+  output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+  output = output.replace(/{%ID%}/g, laptop.id);
+  return output;
+}
